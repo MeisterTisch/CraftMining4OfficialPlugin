954558a87cdfe31c76aca24afe17024b0e953318
BuildTools <unconfigured@null.spigotmc.org>
2023-08-09 10:08:09 +0200

    Pfu-Command Done

--- src/main/java/org/craftmining/craftmining4officialplugin/CraftMining4OfficialPlugin.java
+++ src/main/java/org/craftmining/craftmining4officialplugin/CraftMining4OfficialPlugin.java
@@ -2,6 +2,7 @@ package org.craftmining.craftmining4officialplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,10 +11,10 @@ import org.craftmining.craftmining4officialplugin.elytraBegin.PlayerLandedEvent;
 import org.craftmining.craftmining4officialplugin.fileManagers.BannedPlayersFile;
 import org.craftmining.craftmining4officialplugin.fileManagers.MutedPlayersFile;
 import org.craftmining.craftmining4officialplugin.fileManagers.PlayerManagerFile;
+import org.craftmining.craftmining4officialplugin.fun.PfuCommand;
 
 public final class CraftMining4OfficialPlugin extends JavaPlugin {
     PluginManager pluginManager;
-    private final CraftMining4OfficialPlugin plugin = this;
 
     @Override
     public void onEnable() {
@@ -40,7 +41,8 @@ public final class CraftMining4OfficialPlugin extends JavaPlugin {
         pluginManager.registerEvents(new PlayerLandedEvent(), this);
 
         //COMMANDS
-        getCommand("flyhigh").setExecutor(new FlyHighCommand(plugin));
+        getCommand("flyhigh").setExecutor(new FlyHighCommand(this));
+        getCommand("pfu").setExecutor(new PfuCommand());
     }
 
     @Override
@@ -51,13 +53,21 @@ public final class CraftMining4OfficialPlugin extends JavaPlugin {
     private void checkIfSeasonHasBegun(){
         if(getConfig().getBoolean("hasSeasonBegun")){
             //Season has begun
-            Bukkit.getWorld("world").getWorldBorder().setSize(300000);
+            Bukkit.getWorlds().get(0).getWorldBorder().setSize(300000);
 
         } else {
             //Season hasn't begun yet
-            Bukkit.getWorld("world").getWorldBorder().setCenter(-492.5, -63.5);
-            Bukkit.getWorld("world").getWorldBorder().setSize(10);
-            for(Player player : Bukkit.getOnlinePlayers()) player.teleport(new Location(Bukkit.getWorld("world"), -492.5, 151.1, -63.5, 0,0));
+            Bukkit.getWorlds().get(0).getWorldBorder().setCenter(-492.5, -63.5);
+            Bukkit.getWorlds().get(0).getWorldBorder().setSize(9);
+            for(Player player : Bukkit.getOnlinePlayers()) player.teleport(new Location(Bukkit.getWorlds().get(0), -492.5, 151.1, -63.5, 0,0));
+
+            for(int i = 0; i <= 4; i++){
+                for (int x = -493-i; x <= -493+i; x++) {
+                    for (int z = -64-i; z <= -64+i; z++) {
+                        Bukkit.getWorlds().get(0).getBlockAt(x, 146+i, z).setType(Material.STONE);
+                    }
+                }
+            }
         }
     }
 }
--- src/main/java/org/craftmining/craftmining4officialplugin/fun/PfuCommand.java
+++ src/main/java/org/craftmining/craftmining4officialplugin/fun/PfuCommand.java
@@ -1,13 +1,24 @@
 package org.craftmining.craftmining4officialplugin.fun;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LlamaSpit;
+import org.bukkit.entity.Player;
 
 public class PfuCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        if(sender instanceof Player player){
+            if(args.length == 0){
+                player.getWorld().spawn(player.getEyeLocation(), LlamaSpit.class,
+                        llamaSpit -> llamaSpit.setVelocity(player.getEyeLocation().getDirection().multiply(0.5)));
+            } else player.sendMessage(ChatColor.RED + "Bitte nutze folgenden Command so: \n"
+                                    + ChatColor.GOLD + "/pfu");
+        } else sender.sendMessage(ChatColor.RED + "Diesen Command kann nur ein Spieler ausführen!");
         return true;
     }
 }
